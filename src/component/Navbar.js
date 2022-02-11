ac351712b23aca7c4d3bd8c280333c3147379f70
Wei Loong <nganweiloong@gmail.com>
2022-02-11 12:31:24 +0800

    added navbar and its css

---
+++ src/component/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-center">
+        <div className="logo">
+          <img src="/airasia-logo.svg" />
+        </div>
+        <FontAwesomeIcon size="2x" className="fa-bars" icon={faBars} />
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>About</li>
+          <li>
+            <FontAwesomeIcon icon={faUser} size="" /> Login
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
